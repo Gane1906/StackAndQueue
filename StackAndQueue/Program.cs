6d1702292c098829cf0d9bd0713151a811d682fc
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-03-22 12:12:09 +0530

    [Ganesh] Add:Stack and Queue file is added

---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace StackAndQueue
+{
+    public class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to stack and Queue implementation!");
+        }
+    }
+}
