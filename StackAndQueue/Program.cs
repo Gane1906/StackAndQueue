5d7966b8dba74d795dd38b46ab26e274d062df61
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-03-22 12:32:52 +0530

    [Ganesh] Add:Enqueue method is added

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -5,13 +5,18 @@ namespace StackAndQueue
     {
         static void Main(String[] args)
         {
-            StackImplementation stack = new StackImplementation();
+            /*StackImplementation stack = new StackImplementation();
             stack.Push(70);
             stack.Push(30);
             stack.Push(56);
             stack.Display();
             stack.Peek();
-            stack.Pop();
+            stack.Pop();*/
+            QueueImplementation queue=new QueueImplementation();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display();
         }
     }
 }
---
+++ StackAndQueue/QueueImplementation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class QueueImplementation
+    {
+        public Node head;
+        public void Enqueue(int value)
+        {
+            Node node = new Node(value);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into Queue", node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
